6d74c3d271a1e648a4bdfe1846e28843a2c21016
kvana <kvanadev1@gmail.com>
2015-09-04 15:38:37 +0530

    Initial Commit

---
+++ Example-2Tests/Example_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Example_2Tests.swift
+//  Example-2Tests
+//
+//  Created by Kvana Inc 2 on 04/09/15.
+//  Copyright (c) 2015 Kvana Inc 2. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Example_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
